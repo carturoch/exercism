e2c1895521f6d65b03a900cc0c52aaf7edce2c90
Carlos Cuellar <carturoch@gmail.com>
2018-02-25 10:50:23 -0500

    Add first iteration for largest series product in Go

---
+++ go/largest-series-product/lsproduct.go
@@ -0,0 +1,59 @@
+package lsproduct
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+)
+
+func charToDigit(c byte) (digit int) {
+	digit, _ = strconv.Atoi(string(c))
+	return
+}
+
+func productOf(sequence string, start, span int) (prod int) {
+	prod = charToDigit(sequence[start])
+	for i := start + 1; i < start+span; i++ {
+		prod *= charToDigit(sequence[i])
+	}
+	return
+}
+
+func validateParams(sequence string, span int) (bool, error) {
+	size := len(sequence)
+	if span < 0 {
+		return false, errors.New("Span must be positive")
+	}
+	if span > size {
+		return false, errors.New("Span can't exceed the length of the digits")
+	}
+	if size == 0 || span == 0 {
+		return false, nil
+	}
+	if re := regexp.MustCompile("\\D"); re.MatchString(sequence) {
+		return false, errors.New("Invalid character in the sequence")
+	}
+	return true, nil
+}
+
+// LargestSeriesProduct calculates the largest product of a series
+// of length span in the given sequence of digits
+func LargestSeriesProduct(sequence string, span int) (int, error) {
+	size := len(sequence)
+	res, err := validateParams(sequence, span)
+	if err != nil {
+		return -1, err
+	}
+	if !res {
+		return 1, nil
+	}
+
+	upper := size - span + 1
+	largest := productOf(sequence, 0, span)
+	for i := 1; i < upper; i++ {
+		if cur := productOf(sequence, i, span); cur > largest {
+			largest = cur
+		}
+	}
+	return largest, nil
+}
